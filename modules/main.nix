51ab1fee0351e643e49683c171f379e7b192a031
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-25 01:01:15 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -17,11 +17,12 @@ let
   };
 in
 {
-  options.programs.wallpaper-changer.startup = {
-    folder = lib.mkOption {
+   options.programs.wallpaper-changer.folder = lib.mkOption {
        type = lib.types.str;
        description = "The folder containing wallpapers for the wallpaper changer program.";
        default = "/path/to/wallpapers";  # Replace with the default folder path
     };
+  options.programs.wallpaper-changer.startup = {
+  
   };
 }
